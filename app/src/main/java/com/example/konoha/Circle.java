47dcb5f29b9b8ccf71f1d7504380185e72e25eaa
YohannesGobezie <yohannes.gobezie@gmail.com>
2025-09-25 17:28:23 -0600

    Added Circle class

---
+++ app/src/main/java/com/example/konoha/Circle.java
@@ -0,0 +1,12 @@
+package com.example.konoha;
+
+import java.awt.Shape;
+
+public class Circle extends Shape {
+    private int radius;
+
+    public Rectangle(int x, int y, int radius){
+        super(x, y);
+        this.radius = radius;
+    }
+}
