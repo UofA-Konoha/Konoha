2ee792c5fb379df785af6b6a17f935e83dfca892
Jwuzinski <jwuzinsk@ualberta.ca>
2025-09-25 19:38:35 -0400

    Circle class fix

--- app/src/main/java/com/example/konoha/Circle.java
+++ app/src/main/java/com/example/konoha/Circle.java
@@ -1,11 +1,10 @@
 package com.example.konoha;
 
-import java.awt.Shape;
 
 public class Circle extends Shape {
     private int radius;
 
-    public Rectangle(int x, int y, int radius){
+    public Circle(int x, int y, int radius){
         super(x, y);
         this.radius = radius;
     }
