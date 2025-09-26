dcc78dd935d9e0a0499962bb3ebf31284bc2a05e
CurtisIsCoding <uwakwe809@gmail.com>
2025-09-25 18:50:48 -0600

    commit

--- app/src/main/java/com/example/konoha/Shape.java
+++ app/src/main/java/com/example/konoha/Shape.java
@@ -3,7 +3,7 @@ package com.example.konoha;
 public abstract class Shape {
     protected int x;
     protected int y;
-    String color = "green";
+    String color = "blue";
 
     public Shape(int x, int y){
         this.x = x;
