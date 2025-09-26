7b198a4fba9b34989938a9e98aeabdbdad37c29a
HoytIseghohi <hiseghoh@ualberta.ca>
2025-09-25 18:34:22 -0600

    Changed Shape colour

--- app/src/main/java/com/example/konoha/Shape.java
+++ app/src/main/java/com/example/konoha/Shape.java
@@ -3,7 +3,7 @@ package com.example.konoha;
 public abstract class Shape {
     protected int x;
     protected int y;
-    String color = "yellow";
+    String color = "red";
 
     public Shape(int x, int y){
         this.x = x;
